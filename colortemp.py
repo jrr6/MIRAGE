9647ec4d3bfe1900002764660ae1fff460b90e80
jrr6 <7482866+jrr6@users.noreply.github.com>
2020-11-07 14:44:50 -0500

    Improve edge detection and generate musical parameters

--- colortemp.py
+++ colortemp.py
@@ -1,50 +1,67 @@
 import math
+import numpy as np
+import colour
+
+# https://stackoverflow.com/questions/38876429/how-to-convert-from-rgb-values-to-color-temperature
+def tempFromRGB(r, g, b):
+    # Assuming sRGB encoded colour values.
+    RGB = np.array([r, g, b])
+
+    # Conversion to tristimulus values.
+    XYZ = colour.sRGB_to_XYZ(RGB / 255)
+
+    # Conversion to chromaticity coordinates.
+    xy = colour.XYZ_to_xy(XYZ)
+
+    # Conversion to correlated colour temperature in K.
+    CCT = colour.xy_to_CCT(xy, 'hernandez1999')
+    return CCT
 
 # https://github.com/neilbartlett/color-temperature/blob/master/index.js
-def colorTemperature2rgb(kelvin):
-    temperature = kelvin / 100.0
-    if (temperature < 66.0):
-        red = 255
-    else:
-        red = temperature - 55.0;
-        red = 351.97690566805693+ 0.114206453784165 * red - 40.25366309332127 * math.log(red);
-        if (red < 0): red = 0
-        if (red > 255): red = 255
-
-    if (temperature < 66.0):
-        green = temperature - 2
-        green = -155.25485562709179 - 0.44596950469579133 * green + 104.49216199393888 * math.log(green)
-        if (green < 0): green = 0;
-        if (green > 255): green = 255
-    else:
-        green = temperature - 50.0
-        green = 325.4494125711974 + 0.07943456536662342 * green - 28.0852963507957 * math.log(green)
-        if (green < 0): green = 0
-        if (green > 255): green = 255
-
-    if (temperature >= 66.0):
-        blue = 255
-    else:
-        if (temperature <= 20.0):
-          blue = 0
-        else:
-          blue = temperature - 10
-          blue = -254.76935184120902 + 0.8274096064007395 * blue + 115.67994401066147 * math.log(blue)
-          if (blue < 0): blue = 0
-          if (blue > 255): blue = 255
-
-    return {'red': round(red), 'blue': round(blue), 'green': round(green)}
-
-def rgb2colorTemperature(red, green, blue):
-    epsilon=0.4
-    minTemperature = 1000
-    maxTemperature = 40000
-    while maxTemperature - minTemperature > epsilon:
-        temperature = (maxTemperature + minTemperature) / 2
-        testRGB = colorTemperature2rgb(temperature)
-        # FIXME: this crashes
-        if ((testRGB['blue'] / testRGB['red']) >= (blue / red)):
-            maxTemperature = temperature
-        else:
-            minTemperature = temperature
-    return round(temperature)
+# def colorTemperature2rgb(kelvin):
+#     temperature = kelvin / 100.0
+#     if (temperature < 66.0):
+#         red = 255
+#     else:
+#         red = temperature - 55.0;
+#         red = 351.97690566805693+ 0.114206453784165 * red - 40.25366309332127 * math.log(red);
+#         if (red < 0): red = 0
+#         if (red > 255): red = 255
+#
+#     if (temperature < 66.0):
+#         green = temperature - 2
+#         green = -155.25485562709179 - 0.44596950469579133 * green + 104.49216199393888 * math.log(green)
+#         if (green < 0): green = 0;
+#         if (green > 255): green = 255
+#     else:
+#         green = temperature - 50.0
+#         green = 325.4494125711974 + 0.07943456536662342 * green - 28.0852963507957 * math.log(green)
+#         if (green < 0): green = 0
+#         if (green > 255): green = 255
+#
+#     if (temperature >= 66.0):
+#         blue = 255
+#     else:
+#         if (temperature <= 20.0):
+#           blue = 0
+#         else:
+#           blue = temperature - 10
+#           blue = -254.76935184120902 + 0.8274096064007395 * blue + 115.67994401066147 * math.log(blue)
+#           if (blue < 0): blue = 0
+#           if (blue > 255): blue = 255
+#
+#     return {'red': round(red), 'blue': round(blue), 'green': round(green)}
+#
+# def rgb2colorTemperature(red, green, blue):
+#     epsilon=0.4
+#     minTemperature = 1000
+#     maxTemperature = 40000
+#     while maxTemperature - minTemperature > epsilon:
+#         temperature = (maxTemperature + minTemperature) / 2
+#         testRGB = colorTemperature2rgb(temperature)
+#         # FIXME: this crashes
+#         if ((testRGB['blue'] / testRGB['red']) >= (blue / red)):
+#             maxTemperature = temperature
+#         else:
+#             minTemperature = temperature
+#     return round(temperature)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import math
+import colour
 
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+from colortemp import tempFromRGB
+import colorsys
 
 # imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # # ret, thresh = cv2.threshold(imgray, 50, 255, 0)
@@ -31,10 +34,25 @@ def fetchTestImage():
     height = int(new_width / im.shape[1] * im.shape[0])
     dim = (new_width, height)
     im = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
     kernel = np.ones((4,4), np.float32) / 16
-    dst = cv2.filter2D(im, -1, kernel)
-    edges = cv2.Canny(dst, 200, 300)
+    blurred = cv2.filter2D(gray, -1, kernel)
+    # blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    # https://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+    v = np.median(im)
+    # apply automatic Canny edge detection using the computed median
+    sigma = 0.33
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    # edges = cv2.Canny(blurred, 200, 300)
+    # edges = cv2.Canny(blurred, 50, 100)
+    edges = cv2.Canny(blurred, lower, upper)
+
+    plt.subplot(121), plt.imshow(blurred, cmap='gray')
+    plt.subplot(122), plt.imshow(edges, cmap='gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
     i = 0
     while len(np.nonzero(edges[i])[0]) == 0:
@@ -55,34 +73,106 @@ def fetchTestImage():
         # print()
     result.pop()
 
-    return im, result
+    h = im.shape[0]
+    w = im.shape[1]
+
+    chordPartitionSize = w // 4
+
+    totalWarmth = 0
+    totalHLS = np.array([0.0, 0.0, 0.0])
+    hlsPartitions = np.zeros((4, 4))  # h, l, s, count
+    totalCount = 0
+
+    # loop over the image, pixel by pixel
+    for x in range(0, w):
+        for y in range(0, h):
+            b, g, r = im[y, x]
+            totalWarmth += min(tempFromRGB(r, g, b), 10000)
+            hls = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
+            totalHLS += hls
+            totalCount += 1
+
+            partitionNumber = x // chordPartitionSize
+            hlsPartitions[partitionNumber] += np.array(hls + (1,))
+            # print(f'{r}, {g}, {b} => {tempFromRGB(r, g, b)}')
+            # print(totalWarmth / totalCount)
+
+    for entry in hlsPartitions:
+        entry /= entry[3]
+
+    averageWarmth = (1 - totalWarmth / totalCount / 10000)      # -> tempo, tonality
+    averageHue = totalHLS[0] / totalCount                       # -> tonality
+    averageLuminance = totalHLS[1] / totalCount                 # -> tonality
+    averageSaturation = totalHLS[2] / totalCount                # -> tempo
+
+    tempo = averageWarmth * 0.3 + averageSaturation * 0.7
+    isMajor = averageSaturation * 0.5 + averageLuminance * 0.5 > 0.5
+    tonics = ['G#', 'D#', 'A#', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'C#']
+    tonic = tonics[round(averageHue * 10)]
+
+    tonicOpts = ["I", "VI", "III+", "i", "vi", "iii"]
+    predominantOpts = ["IV", "iv", "iio"]
+    dominantOpts = ["V", "viio"]
+
+    chords = []
+
+    for chord in range(len(hlsPartitions)):
+        partition = hlsPartitions[chord]
+        hue = partition[0]
+        luminance = partition[1]
+        saturation = partition[2]
+        if chord == 0:
+            tonicIdx = round(hue * 2)
+            if not isMajor: tonicIdx += 3
+            chords.append(tonicOpts[tonicIdx])
+        elif chord == 1:
+            if luminance > 0.7 and saturation < 0.3:  # foggy/gray = jazzy
+                chords.append('iio')
+            elif isMajor:
+                chords.append('IV')
+            else:
+                chords.append('iv')
+        elif chord == 2:
+            if saturation > 0.6 and luminance < 0.3:  # dark + intense = viio
+                chords.append('viio')
+            else:
+                chords.append('V')
+        else:
+            similarityToFirst = np.abs(partition - hlsPartitions[0])
+            if similarityToFirst[0] < 0.2:
+                chords.append(chords[0])
+            else:
+                start = 0 if isMajor else 3
+                resolutionOptions = tonicOpts[start:start + 2]
+                differentOpt = list(set(resolutionOptions) - set(chords[0]))[0]
+                chords.append(differentOpt)
+
+    print(chords)
+    print(f'W: {averageWarmth}\tH: {averageHue}\tL: {averageLuminance}\tS: {averageSaturation}')
+
+    cv2.imshow('test', im)
+    cv2.waitKey(0)
+
+    return {
+        'image': im,
+        'edges': edges,  # array of True/False indicating edges
+        'tempo': tempo,
+        'tonic': tonic,
+        'chords': chords,
+        'major': isMajor
+    }
     # for row in result:
     #     for col in row:
     #         print('*' if col else '-', end='')
     #     print()
 
+fetchTestImage()
 
-result = [[]]
-for yPixel in range(len(edges)):
-    for xPixel in range(len(edges[yPixel])):
-        if edges[yPixel][xPixel] > 0:
-            result[-1].append(True)
-        else:
-            result[-1].append(False)
-    result.append([])
-    # print()
-result.pop()
-
-for row in result:
-    for col in row:
-        print('*' if col else '-', end='')
-    print()
-
-plt.subplot(121),plt.imshow(im,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.subplot(121),plt.imshow(im,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+# plt.show()
 #
 # cv2.imshow('test', im)
 # cv2.waitKey(0)
